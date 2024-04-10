31dafa6c47bbadca58ddfd6fdb344956eee4e2cf
Viktor Vodnev <v.vodnev73@gmail.com>
2024-04-10 14:22:10 +0200

    Use package manager to install docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,14 @@ resource "aws_security_group" "allow_docker_tls" {
   description = "Allow TLS inbound traffic and outbound traffic"
   vpc_id      = aws_vpc.docker_vpc.id
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "allow_docker_tls"
   }
